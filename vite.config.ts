4acbbe9806a9e3a38ba2d9f6191b56515f16e200
A . Yasiru Nadeesha Aththanayaka <yasiru.20232389@iit.ac.lk>
2025-06-17 21:43:02 +0530

    Change port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5874,
+    host: '127.0.0.1'
+  }
 })
